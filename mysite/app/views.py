d7b6d8d34b6f45304ad0f02826a31edf0cd0aa27
Khaled MILI <khaled.mili@epita.fr>
2023-02-25 18:09:07 +0100

    adjust database and cleaning views

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
-
-from django.http import HttpResponse
+from .models import Animal
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're in the app index.")
+class IndexView(generic.ListView):
+    template_name = 'app/index.html'
+    context_object_name = 'latest_animal_list'
+
+    def get_queryset(self):
+        """Return the last five published animals."""
+        return Animal.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Animal
+    template_name = 'app/detail.html'
+
+def choose(request, animal_id):
+    return HttpResponse("You're choosing animal %s." % animal_id)
