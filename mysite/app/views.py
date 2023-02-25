2a154cce25418ab973e0232092e8b1f217b6051a
Khaled MILI <khaled.mili@epita.fr>
2023-02-25 18:42:24 +0100

    adding style sheet

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Animal
 
@@ -12,12 +13,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published animals."""
-        return Animal.objects.order_by('-pub_date')[:5]
+        return
+    Animal.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Animal
     template_name = 'app/detail.html'
+    def get_queryset(self):
+        """Excludes any animals that aren't published yet"""
+        return Animal.objects.filter(pub_date__lte=timezone.now())
 
 def choose(request, animal_id):
     return HttpResponse("You're choosing animal %s." % animal_id)
