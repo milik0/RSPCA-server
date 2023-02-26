7a54282e46a09ca79a5a2ada2b967987cf45aa66
Khaled MILI <khaled.mili@epita.fr>
2023-02-26 16:38:31 +0100

    front

--- mysite/app/admin.py
+++ mysite/app/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Animal
 
-admin.site.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None,               {'fields': ['species']}),
+            ('Date information', {'fields': ['pub_date']}),
+    ]
+
+admin.site.register(Animal, AnimalAdmin)
